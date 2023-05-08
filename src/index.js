3ce5ae23cfb0b9206d1255c8ff0cc36a96167ee2
omrialloro <omrialloro@gmail.com>
2023-05-08 21:43:13 +0300

    ss

--- src/index.js
+++ src/index.js
@@ -265,74 +265,7 @@ app.post("/deleteStoredAnimation", checkJwt, (request, response) => {
   // var data_str = JSON.stringify(request.body)
 });
 
-// app.post("/gif", checkJwt, async (req, res) => {
-//   const { frames, delay } = req.body;
-//   const num_pixels = frames[0].length;
-//   const pixel_size = 10;
-//   const margin = 1;
-//   const size_frame = pixel_size * num_pixels + 2 * margin * (num_pixels + 1);
-
-//   const encoder = new GIFEncoder(size_frame, size_frame);
-
-//   encoder.start();
-//   encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
-//   encoder.setDelay(delay); // frame delay in ms
-//   encoder.setQuality(20); //
-
-//   for (let i = 0; i < frames.length; i++) {
-//     encoder.addFrame(Parser(frames[i]));
-//   }
-
-//   const gifData = encoder.out.getData();
-
-//   res.writeHead(200, {
-//     "Content-Type": "image/gif",
-//     "Content-Disposition": 'attachment; filename="mygif.gif"',
-//     // "Access-Control-Allow-Origin": "*", // allow requests from any origin
-//     // "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-//   });
-//   res.end(Buffer.from(gifData));
-
-//   encoder.finish();
-// });
-
-// app.post("/gif", checkJwt, async (req, res) => {
-//   try {
-//     const { frames, delay } = req.body;
-//     const num_pixels = frames[0].length;
-//     const pixel_size = 10;
-//     const margin = 1;
-//     const size_frame = pixel_size * num_pixels + 2 * margin * (num_pixels + 1);
-
-//     const encoder = new GIFEncoder(size_frame, size_frame);
-
-//     encoder.start();
-//     encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
-//     encoder.setDelay(delay); // frame delay in ms
-//     encoder.setQuality(20); //
-
-//     for (let i = 0; i < frames.length; i++) {
-//       encoder.addFrame(Parser(frames[i]));
-//     }
-
-//     const gifData = encoder.out.getData();
-
-//     res.writeHead(200, {
-//       "Content-Type": "image/gif",
-//       "Content-Disposition": 'attachment; filename="mygif.gif"',
-//     });
-//     res.end(Buffer.from(gifData));
-
-//     encoder.finish();
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).send({ error: "Internal Server Error" });
-//   }
-// });
-
 app.post("/gif", checkJwt, async (req, res) => {
-  var data = JSON.stringify(req.body);
-
   const { frames, delay } = req.body;
   const num_pixels = frames[0].length;
   const pixel_size = 10;
@@ -352,22 +285,90 @@ app.post("/gif", checkJwt, async (req, res) => {
 
   const gifData = encoder.out.getData();
 
-  var animationId = String(Date.now());
-  console.log(
-    `https://dlb-thumbnails.s3.eu-central-1.amazonaws.com/frames/${animationId}.json`
-  );
+  res.writeHead(200, {
+    "Content-Type": "image/gif",
+    "Content-Disposition": 'attachment; filename="mygif.gif"',
+    // "Access-Control-Allow-Origin": "*", // allow requests from any origin
+    // "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  });
+  res.end(Buffer.from(gifData));
 
-  console.log("DDDD");
-
-  await s3
-    .putObject({
-      Bucket: "dlb-thumbnails",
-      Key: `frames/${animationId}.json`,
-      Body: data,
-      ContentType: "application/json",
-    })
-    .promise();
-  return res.send();
+  encoder.finish();
 });
+
+app.post("/gif", checkJwt, async (req, res) => {
+  try {
+    const { frames, delay } = req.body;
+    const num_pixels = frames[0].length;
+    const pixel_size = 10;
+    const margin = 1;
+    const size_frame = pixel_size * num_pixels + 2 * margin * (num_pixels + 1);
+
+    const encoder = new GIFEncoder(size_frame, size_frame);
+
+    encoder.start();
+    encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
+    encoder.setDelay(delay); // frame delay in ms
+    encoder.setQuality(20); //
+
+    for (let i = 0; i < frames.length; i++) {
+      encoder.addFrame(Parser(frames[i]));
+    }
+
+    const gifData = encoder.out.getData();
+
+    res.writeHead(200, {
+      "Content-Type": "image/gif",
+      "Content-Disposition": 'attachment; filename="mygif.gif"',
+    });
+    res.end(Buffer.from(gifData));
+
+    encoder.finish();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
+// app.post("/gif", checkJwt, async (req, res) => {
+//   var data = JSON.stringify(req.body);
+
+//   const { frames, delay } = req.body;
+//   const num_pixels = frames[0].length;
+//   const pixel_size = 10;
+//   const margin = 1;
+//   const size_frame = pixel_size * num_pixels + 2 * margin * (num_pixels + 1);
+
+//   const encoder = new GIFEncoder(size_frame, size_frame);
+
+//   encoder.start();
+//   encoder.setRepeat(0); // 0 for repeat, -1 for no-repeat
+//   encoder.setDelay(delay); // frame delay in ms
+//   encoder.setQuality(20); //
+
+//   for (let i = 0; i < frames.length; i++) {
+//     encoder.addFrame(Parser(frames[i]));
+//   }
+
+//   const gifData = encoder.out.getData();
+//   console.log(gifData);
+
+//   var animationId = String(Date.now());
+//   console.log(
+//     `https://dlb-thumbnails.s3.eu-central-1.amazonaws.com/frames/${animationId}.json`
+//   );
+
+//   console.log("DDDD");
+
+//   await s3
+//     .putObject({
+//       Bucket: "dlb-thumbnails",
+//       Key: `frames/${animationId}.json`,
+//       Body: data,
+//       ContentType: "application/json",
+//     })
+//     .promise();
+//   return res.send();
+// });
 
 exports.handler = serverlessExpress({ app });
