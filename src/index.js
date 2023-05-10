0a8523c533b63e1f9a52be92e510e38db9a09f3c
omrialloro <omrialloro@gmail.com>
2023-05-10 18:36:03 +0300

    s

--- src/index.js
+++ src/index.js
@@ -340,6 +340,8 @@ app.post("/gif", checkJwt, async (req, res) => {
 
     const gifData = encoder.out.getData();
 
+    console.log(gifData);
+
     res.writeHead(200, {
       "Content-Type": "image/gif",
       "Content-Disposition": 'attachment; filename="mygif.gif"',
