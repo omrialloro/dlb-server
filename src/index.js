0d0bd06d10eb791fb2d2907d2e913b84975e1ffb
omrialloro <omrialloro@gmail.com>
2023-05-12 02:22:36 +0300

    s

--- src/index.js
+++ src/index.js
@@ -368,7 +368,7 @@ app.post("/gif", checkJwt, async (req, res) => {
     console.error(error);
     res.status(500).send({ error: "Internal Server Error" });
   }
-  return res.send();
+  // return res.send();
 });
 
 // app.post("/gif", checkJwt, async (req, res) => {
